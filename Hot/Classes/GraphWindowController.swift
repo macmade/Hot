f6e9275e2c65fa611062632063e7da2454f7ce4c
macmade <macmade@xs-labs.com>
2023-02-06 23:30:29 +0100

    Graph panel can now be shown on all spaces.

--- Hot/Classes/GraphWindowController.swift
+++ Hot/Classes/GraphWindowController.swift
@@ -27,13 +27,22 @@ import Foundation
 @objc
 public class GraphWindowController: NSWindowController
 {
-    @IBOutlet public private( set ) var graphView: GraphView?
+    @IBOutlet public private( set ) var graphView:   GraphView?
 
-    @objc public dynamic var schedulerLimit:  Int = 0
-    @objc public dynamic var availableCPUs:   Int = 0
-    @objc public dynamic var speedLimit:      Int = 0
-    @objc public dynamic var temperature:     Int = 0
-    @objc public dynamic var thermalPressure: Int = 0
+    @objc public dynamic var schedulerLimit:  Int  = 0
+    @objc public dynamic var availableCPUs:   Int  = 0
+    @objc public dynamic var speedLimit:      Int  = 0
+    @objc public dynamic var temperature:     Int  = 0
+    @objc public dynamic var thermalPressure: Int  = 0
+    @objc public dynamic var showOnAllSpaces: Bool = false
+    {
+        didSet
+        {
+            UserDefaults.standard.set( self.showOnAllSpaces, forKey: "showGraphPanelOnAllSpaces" )
+
+            self.window?.collectionBehavior = self.showOnAllSpaces ? .canJoinAllSpaces : []
+        }
+    }
 
     #if arch( arm64 )
         @objc public private( set ) dynamic var isARM = true
@@ -59,5 +68,25 @@ public class GraphWindowController: NSWindowController
     public override func windowDidLoad()
     {
         super.windowDidLoad()
+
+        if UserDefaults.standard.bool( forKey: "showGraphPanelOnAllSpaces" )
+        {
+            self.showOnAllSpaces = true
+        }
+    }
+
+    @IBAction
+    private func showOptions( _ sender: Any? )
+    {
+        let menu = NSMenu()
+        let item = menu.addItem( withTitle: "Show on All Spaces", action: nil, keyEquivalent: "" )
+
+        item.bind( NSBindingName( "value" ), to: self, withKeyPath: "showOnAllSpaces" )
+
+        if let view  = sender as? NSView,
+           let event = NSApp.currentEvent
+        {
+            NSMenu.popUpContextMenu( menu, with: event, for: view )
+        }
     }
 }
